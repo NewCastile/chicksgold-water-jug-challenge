f5e9c7be1cc69db213154bc2928b64900d1f0eca
NewCastile <carlosaguanipa1@gmail.com>
2024-04-28 21:27:49 -0400

    fixed stringifiedTrackSteps update

--- src/hooks/useTrack.tsx
+++ src/hooks/useTrack.tsx
@@ -19,24 +19,28 @@ export const useTrack = ({
     return trackSteps.length
   }, [trackSteps])
 
+  // Keeps an strinfied version of the steps taken to make the comparison easier
   const [stringifiedTrackSteps, setStringifiedtrackSteps] = useState<string[]>(
     []
   )
 
+  // Adds the stringified version of last step taken to the stringified steps array
   useEffect(() => {
     if (trackSteps.length > 0) {
-      const lastTrackStep = trackSteps.slice(-1)
+      const [lastTrackStep] = trackSteps.slice(-1)
       const stringifedLastTrackStep = JSON.stringify(lastTrackStep)
 
       setStringifiedtrackSteps((old) => old.concat(stringifedLastTrackStep))
     }
   }, [trackSteps])
 
+  // Computes the next track step to complete the goal and stops the loop from continuing after achieving the goal
   useEffect(() => {
-    if (stopCondition) {
+    // Prevents the loop from continuing after it completes the goal
+    if (stopCondition || trackIsFinished) {
       return
     }
-    if (trackIsFinished) return
+
     if (iterationIndex < MAX_ITERATIONS) {
       if (isEmpty(trackSteps)) return
 
@@ -44,22 +48,25 @@ export const useTrack = ({
 
       const [trackLastStep] = trackSteps.slice(-1)
 
+      // Ends the loop in case one of the buckets has the targeted gallons
       if (
         trackLastStep.some((bucket) => bucket.currentGallons === targetCapacity)
       ) {
         setTrackIsFinished(true)
+
+        return
       }
 
-      const [lastTrackABucketX, lastTrackABucketY] = trackLastStep
+      const [lastTrackBucketX, lastTrackBucketY] = trackLastStep
 
       const nextTrackStep = transfer({
-        pouringBucket: lastTrackABucketX,
-        toFillBucket: lastTrackABucketY,
+        pouringBucket: lastTrackBucketX,
+        toFillBucket: lastTrackBucketY,
       })
 
       const stringifiedNextTrackStep = JSON.stringify(nextTrackStep)
 
-      // End the loop in case the next step has been taken
+      // Ends the loop in case the next step has been taken
       if (
         stringifiedTrackSteps.some(
           (trackStepTaken) => trackStepTaken === stringifiedNextTrackStep
@@ -70,6 +77,7 @@ export const useTrack = ({
 
       const [newTrackBucketX, newTrackBucketY] = nextTrackStep
 
+      // Adds the new step to the array of steps and continues the loop
       setTrackSteps((old) => old.concat([[newTrackBucketX, newTrackBucketY]]))
     }
   }, [
@@ -81,6 +89,7 @@ export const useTrack = ({
     trackSteps,
   ])
 
+  // updates the loop index when the track's steps changes
   useEffect(() => {
     setIterationsIndex((old) => old + 1)
   }, [trackSteps])
