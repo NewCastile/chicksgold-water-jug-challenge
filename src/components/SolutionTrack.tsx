600142a00b106902d6e87ac0debd1297ae449cb1
NewCastile <carlosaguanipa1@gmail.com>
2024-04-29 14:18:53 -0400

    update for trackStepsTaken rendering

--- src/components/SolutionTrack.tsx
+++ src/components/SolutionTrack.tsx
@@ -5,7 +5,7 @@ export const SolutionTrack = ({ track }: ISolutionTrackProps) => {
     <pre className={"flex w-full flex-col items-center justify-center gap-8"}>
       <pre>{track.trackProcessDescription}</pre>
       {track.trackStepsTaken > 0 && (
-        <pre>Steps taken: {track.trackStepsTaken - 1}</pre>
+        <pre>Steps taken: {track.trackStepsTaken}</pre>
       )}
       <div className={"grid w-full grid-flow-row grid-cols-3 gap-4"}>
         {track.trackSteps.map((step, stepIdx) => {
