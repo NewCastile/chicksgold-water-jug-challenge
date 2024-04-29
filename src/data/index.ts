c917bb3258e3beae2e55b2fb7cb9ef922e39a1a4
NewCastile <carlosaguanipa1@gmail.com>
2024-04-29 16:18:15 -0400

    added solution track mock data as constant

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,4 +1,4 @@
-import { IOperableBucket } from "../types"
+import { IOperableBucket, TTrackProps } from "../types"
 
 export const POURING_BUCKET_TEMPLATE: IOperableBucket = {
   tag: "pouring-bucket",
@@ -10,6 +10,12 @@ export const TO_FILL_BUCKET_TEMPLATE: IOperableBucket = {
   tag: "to-fill-bucket",
   capacity: 0,
   currentGallons: 0,
+}
+
+export const SOLUTION_TRACK_MOCK_DATA: TTrackProps = {
+  trackProcessDescription: "Fill X then transfer to Y",
+  trackSteps: [],
+  trackStepsTaken: 0,
 }
 
 export const MAX_ITERATIONS = 56
