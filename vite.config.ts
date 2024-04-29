96ed4db395945f1462e1a3129afc075e0a0e7018
NewCastile <carlosaguanipa1@gmail.com>
2024-04-29 14:11:38 -0400

    update for vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
+/// <reference types="vitest" />
+
 import react from "@vitejs/plugin-react-swc"
 import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    exclude: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/cypress/**",
+      "**/.{idea,git,cache,output,temp}/**",
+      "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*",
+    ],
+    environment: "happy-dom",
+  },
 })
