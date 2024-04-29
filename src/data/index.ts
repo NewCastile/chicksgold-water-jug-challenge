1a228c2687f5854d230bf3eefcea4a0bd7051090
NewCastile <carlosaguanipa1@gmail.com>
2024-04-28 21:34:00 -0400

    update for typings

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,12 +1,12 @@
-import { OperableBucket } from "../types"
+import { IOperableBucket } from "../types"
 
-export const POURING_BUCKET_TEMPLATE: OperableBucket = {
+export const POURING_BUCKET_TEMPLATE: IOperableBucket = {
   tag: "pouring-bucket",
   capacity: 0,
   currentGallons: 0,
 }
 
-export const TO_FILL_BUCKET_TEMPLATE: OperableBucket = {
+export const TO_FILL_BUCKET_TEMPLATE: IOperableBucket = {
   tag: "to-fill-bucket",
   capacity: 0,
   currentGallons: 0,
