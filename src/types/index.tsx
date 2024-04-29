c5a2d4afee370bb07fddec324aa76072669e980e
NewCastile <carlosaguanipa1@gmail.com>
2024-04-28 20:36:03 -0400

    added new typings

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -1,19 +1,53 @@
-export interface Bucket {
+export interface IBucket {
   tag: string
   capacity: number
 }
 
-export interface OperableBucket extends Bucket {
+export interface IOperableBucket extends IBucket {
   currentGallons: number
 }
 
-export interface ProblemParameters {
+export interface IProblemParameters {
   bucketXCapacity: number
   bucketYCapacity: number
   targetCapacity: number
 }
 
-export interface FormValidationResult {
+export interface IFormValidationResult {
   success: boolean
   message: string
 }
+
+export interface ITrack {
+  trackSteps: IOperableBucket[][]
+  setTrackSteps: React.Dispatch<React.SetStateAction<IOperableBucket[][]>>
+  trackProcessDescription: string
+  trackStepsTaken: number
+  trackIsFinished?: boolean
+}
+
+export interface IErrorMessageProps {
+  error: string
+  handleErroMessageButtonOnClick: (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => void
+}
+
+export interface IUseTrackArguments {
+  target: IBucket
+  stopCondition: boolean
+  trackProcessDescription: string
+}
+
+export interface IFormProps {
+  disabled: boolean
+  handleFormSubmit: (event: React.FormEvent<HTMLFormElement>) => void
+}
+
+export interface ISolutionTrackProps {
+  track: TTrackProps
+}
+
+export type TSolutionTrack = Omit<ITrack, "setTrackSteps" | "trackIsFinished">
+
+export type TTrackProps = Omit<ITrack, "setTrackSteps">
