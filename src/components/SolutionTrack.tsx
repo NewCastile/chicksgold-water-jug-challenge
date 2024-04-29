fabb03b079f6a6135eb152c1d5f8a7b50b5f2820
NewCastile <carlosaguanipa1@gmail.com>
2024-04-28 22:00:15 -0400

    update for styling

--- src/components/SolutionTrack.tsx
+++ src/components/SolutionTrack.tsx
@@ -5,49 +5,64 @@ export const SolutionTrack = ({ track }: ISolutionTrackProps) => {
     <pre className={"flex w-full flex-col items-center justify-center gap-8"}>
       <pre>{track.trackProcessDescription}</pre>
       {track.trackStepsTaken > 0 && (
-        <pre>Steps taken: {track.trackStepsTaken}</pre>
+        <pre>Steps taken: {track.trackStepsTaken - 1}</pre>
       )}
-      {track.trackSteps.map((step, stepIdx) => {
-        const [a, b] = step
-        const {
-          tag: aTag,
-          currentGallons: aCurrentGallons,
-          capacity: aCapacity,
-        } = a
-        const {
-          tag: bTag,
-          currentGallons: bCurrentGallons,
-          capacity: bCapacity,
-        } = b
+      <div className={"grid w-full grid-flow-row grid-cols-3 gap-4"}>
+        {track.trackSteps.map((step, stepIdx) => {
+          const [a, b] = step
+          const {
+            tag: aTag,
+            currentGallons: aCurrentGallons,
+            capacity: aCapacity,
+          } = a
+          const {
+            tag: bTag,
+            currentGallons: bCurrentGallons,
+            capacity: bCapacity,
+          } = b
 
-        return (
-          <div
-            key={stepIdx}
-            className={"flex flex-row items-center justify-center gap-8"}
-          >
-            <div className={"flex flex-col items-center justify-center gap-2"}>
-              <div>{aTag}</div>
+          return (
+            <div
+              key={stepIdx}
+              className={
+                "relative flex flex-row items-center justify-center gap-8"
+              }
+            >
               <div
-                className={"flex flex-row items-center justify-center gap-2"}
+                className={
+                  "absolute left-0 top-0 inline-flex size-6 items-center justify-center rounded-full border-2 border-white align-middle"
+                }
               >
-                <div>{aCurrentGallons}</div>
-                <div>/</div>
-                <div>{aCapacity}</div>
+                {stepIdx}
+              </div>
+              <div
+                className={"flex flex-col items-center justify-center gap-2"}
+              >
+                <div>{aTag}</div>
+                <div
+                  className={"flex flex-row items-center justify-center gap-2"}
+                >
+                  <div>{aCurrentGallons}</div>
+                  <div>/</div>
+                  <div>{aCapacity}</div>
+                </div>
+              </div>
+              <div
+                className={"flex flex-col items-center justify-center gap-4"}
+              >
+                <div>{bTag}</div>
+                <div
+                  className={"flex flex-row items-center justify-center gap-2"}
+                >
+                  <div>{bCurrentGallons}</div>
+                  <div>/</div>
+                  <div>{bCapacity}</div>
+                </div>
               </div>
             </div>
-            <div className={"flex flex-col items-center justify-center gap-4"}>
-              <div>{bTag}</div>
-              <div
-                className={"flex flex-row items-center justify-center gap-2"}
-              >
-                <div>{bCurrentGallons}</div>
-                <div>/</div>
-                <div>{bCapacity}</div>
-              </div>
-            </div>
-          </div>
-        )
-      })}
+          )
+        })}
+      </div>
     </pre>
   )
 }
